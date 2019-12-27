23bb92cf0ccb11bc56b23bb6921da16434d3e737
Krzysztof Dziardziel <krzysztof.dziardziel@students.akademeia.edu.pl>
2019-12-28 00:49:14 +0100

    v1.0.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,51 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNum = 1;
+  int rightDiceNum = 1;
+  void randomizeDie() {
+    rightDiceNum = Random().nextInt(6) + 1;
+    leftDiceNum = Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 20),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    randomizeDie();
+                  });
+                },
+                child: Image.asset('images/dice$leftDiceNum.png'),
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  setState(
+                    () {
+                      randomizeDie();
+                    },
+                  );
+                },
+                child: Image.asset('images/dice$rightDiceNum.png'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
